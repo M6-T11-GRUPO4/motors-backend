c6fba96d10d3e453d5532de29c4cd558ce343d65
Bruno Tiberio <quentemente@gmail.com>
2022-12-20 13:44:09 -0300

    feat: delete e softdelete vehicle

--- src/routes/vehicle.routes.ts
+++ src/routes/vehicle.routes.ts
@@ -3,11 +3,12 @@ import yupValidateMiddleware from "../middlewares/yupValidate.middleware";
 import postVehicleSchema from "../schemas/postVehicle.schema";
 
 import createVehicleController from "../controllers/vehicle/createVehicle.controller";
-import deleteVehicleController from "../controllers/vehicle/deleteVehicle.controller";
 import listAllVehiclesController from "../controllers/vehicle/listAllVehicles.controller";
 import listImagesByIdVehicleController from "../controllers/vehicle/listImagesByIdVehicle.controller";
 import listVehicleByIdController from "../controllers/vehicle/listVehicleById.controller";
 import updateVehicleController from "../controllers/vehicle/updateVehicle.controller";
+import softDeleteVehicleController from "../controllers/vehicle/softDeleteVehicle.controller";
+import deleteVehicleController from "../controllers/vehicle/deleteVehicle.controller";
 
 const vehicleRouter = Router();
 
@@ -19,6 +20,7 @@ vehicleRouter.get("/:id/images", listImagesByIdVehicleController);
 
 vehicleRouter.patch("/:id", updateVehicleController);
 
-vehicleRouter.delete("/:id/delete", deleteVehicleController);
+vehicleRouter.delete("/:id/deactivate", softDeleteVehicleController);
+vehicleRouter.delete('/:id/delete', deleteVehicleController)
 
 export default vehicleRouter;
