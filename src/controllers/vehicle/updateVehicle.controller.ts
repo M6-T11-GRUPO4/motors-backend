22d551fb5f10d71284b75773bc0b10105659b7fa
Bruno Tiberio <quentemente@gmail.com>
2022-12-20 08:38:01 -0300

    feat: update vehicle

---
+++ src/controllers/vehicle/updateVehicle.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { IVehiclePatchRequest } from "../../interfaces";
+import updateVehicleService from "../../services/vehicle/updateVehicle.service";
+
+const updateVehicleController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const {
+    description,
+    km,
+    name,
+    price,
+    type,
+    user_mokado,
+    year,
+    is_active
+  }: IVehiclePatchRequest = req.body;
+
+  const updateVehicle = await updateVehicleService(
+    id,
+    description,
+    km,
+    name,
+    price,
+    type,
+    user_mokado,
+    year,
+    is_active
+  );
+
+  return res.status(200).json(updateVehicle);
+};
+
+export default updateVehicleController;
