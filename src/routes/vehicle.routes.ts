21e2c62cb9ae0e9898ec75b77b35a01b829fbfdb
Bruno Tiberio <quentemente@gmail.com>
2022-12-20 08:38:28 -0300

    feat: rotas de vehicle adicionadas

--- src/routes/vehicle.routes.ts
+++ src/routes/vehicle.routes.ts
@@ -1,8 +1,24 @@
 import { Router } from "express";
+
 import createVehicleController from "../controllers/vehicle/createVehicle.controller";
+import deleteVehicleController from "../controllers/vehicle/deleteVehicle.controller";
+import listAllVehiclesController from "../controllers/vehicle/listAllVehicles.controller";
+import listImagesByIdVehicleController from "../controllers/vehicle/listImagesByIdVehicle.controller";
+import listVehicleByIdController from "../controllers/vehicle/listVehicleById.controller";
+import updateVehicleController from "../controllers/vehicle/updateVehicle.controller";
 
 const vehicleRouter = Router();
 
 vehicleRouter.post("", createVehicleController);
+
+vehicleRouter.get("", listAllVehiclesController);
+
+vehicleRouter.get("/:id", listVehicleByIdController);
+
+vehicleRouter.patch("/:id", updateVehicleController);
+
+vehicleRouter.patch("/:id/delete", deleteVehicleController);
+
+vehicleRouter.get("/:id/images", listImagesByIdVehicleController);
 
 export default vehicleRouter;
