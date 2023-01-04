33ec2e75531d637147a6f2c05613edb83073a8d8
Bruno Tiberio <quentemente@gmail.com>
2023-01-04 11:46:00 -0300

    feat: rotas de usuário

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 import verifyEmailAndCpfAvailabilityMiddleware from "../middlewares/verifyEmailAndCpfAvailability.middleware";
 
 import createUserController from "../controllers/user/createUser.controller";
+import listUserByIdController from "../controllers/user/listUserById.controller";
+import listVehiclesByIdUserController from "../controllers/user/listVehiclesByIdUser.controller";
+import updateUserController from "../controllers/user/updateUser.controller";
+import deleteUserController from "../controllers/user/deleteUser.controller";
 
 const userRouter = Router();
 
@@ -10,5 +14,13 @@ userRouter.post(
   verifyEmailAndCpfAvailabilityMiddleware,
   createUserController
 );
+
+userRouter.get("/:id", listUserByIdController);
+
+userRouter.patch("/:id", updateUserController);
+
+userRouter.delete("/:id", deleteUserController);
+
+userRouter.get("/:id/vehicles", listVehiclesByIdUserController);
 
 export default userRouter;
