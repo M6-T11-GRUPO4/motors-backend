fa379bfe186f4c3c1c37f083b91dd96738b69c83
Bruno Tiberio <quentemente@gmail.com>
2023-01-04 11:43:00 -0300

    fix: padronização de código

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -13,25 +13,32 @@ const createUserService = async ({
   is_active = true,
   is_seller = false,
 }: IUserRequest) => {
-  // console.log({name, email, password, birthdate, cpf, cellphone, description, is_active, is_seller})
-
   const hashedPassword = await hash(password, 10);
-
-  console.log(hashedPassword);
 
   const user = await prisma.user.create({
     data: {
+      name,
+      email,
+      password: hashedPassword,
+      description,
+      cpf,
       birthdate: new Date(birthdate),
       cellphone,
-      cpf,
-      description,
-      email,
-      name,
-      password: hashedPassword,
+      is_active,
+      is_seller,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      description: true,
+      cpf: true,
+      birthdate: true,
+      cellphone: true,
+      is_active: true,
+      is_seller: true,
     },
   });
-
-  console.log(user);
 
   return user;
 };
