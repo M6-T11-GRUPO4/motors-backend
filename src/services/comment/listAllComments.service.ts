3a1706b9ecf7fd135dc273c150f3d0c0941c8e43
Debora Correa <deboragomescorr@gmail.com>
2023-01-06 11:38:30 -0300

    fix: reverte retorno para ordenar comentários do mais novo ao mais velho

--- src/services/comment/listAllComments.service.ts
+++ src/services/comment/listAllComments.service.ts
@@ -3,7 +3,7 @@ import { prisma } from "../../prismaClient";
 const listAllCommentsService = async () => {
   const comments = await prisma.comment.findMany();
 
-  return comments;
+  return comments.reverse();
 };
 
 export default listAllCommentsService;
