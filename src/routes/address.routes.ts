ca66454f2cf883535532f217f0e8a257f0818006
Debora Correa <deboragomescorr@gmail.com>
2023-01-06 12:20:23 -0300

    feat: lista address por id e requer autorização para patch de address

--- src/routes/address.routes.ts
+++ src/routes/address.routes.ts
@@ -3,11 +3,23 @@ import { Router } from "express";
 import updateAddressController from "../controllers/address/updateAddress.controller";
 import yupValidateMiddleware from "../middlewares/yupValidate.middleware";
 import patchAddressSchema from "../schemas/patchAddress.schema";
+import authTokenMiddleware from "../middlewares/authToken.middleware";
+import userIsHimselfMiddleware from "../middlewares/userIsHimself.middlware";
+import listAddressByIdController from "../controllers/address/listAddressById.controller";
 
 const addressRoutes = Router();
 
+addressRoutes.get(
+  "/:id",
+  authTokenMiddleware,
+  userIsHimselfMiddleware,
+  listAddressByIdController
+);
+
 addressRoutes.patch(
   "/:id",
+  authTokenMiddleware,
+  userIsHimselfMiddleware,
   yupValidateMiddleware(patchAddressSchema),
   updateAddressController
 );
