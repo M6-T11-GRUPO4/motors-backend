7fe2382d00220eaa0e9b1063037d05c11ffefede
Bruno Tiberio <quentemente@gmail.com>
2023-01-10 12:25:52 -0300

    fix: reset password

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -28,6 +28,7 @@ userRouter.post(
 userRouter.post("/login", userLoginController);
 
 userRouter.post('/login/forgot-password', forgotPasswordController)
+userRouter.patch('/login/reset-password', resetPasswordController)
 
 userRouter.get("/:id", listUserByIdController);
 userRouter.get("/:id/vehicles", listVehiclesByIdUserController);
